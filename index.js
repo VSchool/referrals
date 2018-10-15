e72b4820fc92f4e40e426bcc335ff0c39b0bfbaa
Bob Ziroll <elderziroll@gmail.com>
2018-10-15 14:16:24 -0600

    Add bird endpoint

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 8000
 
-const referralCodes = ["RHRFEVD", "R6ULI52", "R626LIU", "RHHKHVN"]
 
 app.get("/", (req, res) => {
     return res.redirect("/lime")
 })
 
 app.get("/lime", (req, res) => {
+    const referralCodes = ["RHRFEVD", "R6ULI52", "R626LIU", "RHHKHVN"]
     const randCode = referralCodes[Math.floor(Math.random() * referralCodes.length)]
     const url = `https://lime.bike/referral/${randCode}`
     return res.redirect(url)
+})
+
+app.get("/bird", (req, res) => {
+    const referralUrls = ["https://gift.bird.co/OhyJTb3J2Q"]
+    const randUrl = referralUrls[Math.floor(Math.random() * referralUrls.length)]
+    return res.redirect(randUrl)
 })
 
 app.listen(PORT, () => {
