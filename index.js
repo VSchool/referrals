697436c04d1e5fbe70c362e321b577691ed9eef9
Bob Ziroll <elderziroll@gmail.com>
2018-10-15 13:48:50 -0600

    Add redirect for base url

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const PORT = process.env.PORT || 8000
 
 const referralCodes = ["RHRFEVD", "R6ULI52", "R626LIU", "RHHKHVN"]
 
+app.get("/", (req, res) => {
+    return res.redirect("/lime")
+})
+
 app.get("/lime", (req, res) => {
     const randCode = referralCodes[Math.floor(Math.random() * referralCodes.length)]
     const url = `https://lime.bike/referral/${randCode}`
-    res.redirect(url)
+    return res.redirect(url)
 })
 
 app.listen(PORT, () => {
